e02903408a0c9cb4ca50078af655a84ef8068284
quechulo <aawangardowy@gmail.com>
2023-05-05 13:01:47 +0200

    more reusable code for dbReader

--- source/dbReader.py
+++ source/dbReader.py
@@ -4,6 +4,57 @@ from passwords import password
 database = 'products'
 collection = 'clothes'
 
+def connect_db(database, collection):
+    try:
+        client = MongoClient(
+            f"mongodb+srv://pyReader:{password}@cluster0.bmmwhmg.mongodb.net/?retryWrites=true&w=majority")
+        print("Connected successfully!")
+    except Exception as e:
+        print("Unable to connect to the cluster: ", e)
+
+    # Access the database and collection
+    try:
+        db = client.get_database(database)
+        collection = db.get_collection(collection)
+        print("Database and collection accessed successfully!")
+    except Exception as e:
+        print("Unable to access database or collection: ", e)
+
+    # Retrieve all documents in the collection
+    try:
+        documents = collection.find()
+        print("Retrieved all documents successfully!")
+        return documents
+
+    except Exception as e:
+        print("Unable to retrieve documents: ", e)
+
+
+def write_to_file(documents, file_name):
+    try:
+        # Open the file in write mode
+        with open(file_name, "w") as file:
+            # Use the truncate() method to erase the contents
+            file.truncate()
+        # Iterate through each document and print its contents
+        for doc in documents:
+            product = {
+                'type_of_product': doc['type'],
+                'brand': doc['brand'],
+                'description': doc['description'],
+                'name': doc['name'],
+                'photos': doc['photos'],
+                'color': [doc['color']],
+                'gender': doc['gender'],
+                'price': doc['price'],
+                'link': doc['_id'],  # TODO add link to product
+            }
+
+            add_dict_to_file(file_name, product)
+    except Exception as e:
+        print("Error processing document: ", e)
+
+
 def add_dict_to_file(file_path, product):
     category = {
         'sneakers': 'buty',
@@ -40,44 +91,8 @@ def add_dict_to_file(file_path, product):
 
 
 # Set up the connection to the MongoDB Atlas database
-try:
-    client = MongoClient(f"mongodb+srv://pyReader:{password}@cluster0.bmmwhmg.mongodb.net/?retryWrites=true&w=majority")
-    print("Connected successfully!")
-except Exception as e:
-    print("Unable to connect to the cluster: ", e)
-
-# Access the database and collection
-try:
-    db = client.get_database(database)
-    collection = db.get_collection(collection)
-    print("Database and collection accessed successfully!")
-except Exception as e:
-    print("Unable to access database or collection: ", e)
-
-# Retrieve all documents in the collection
-try:
-    documents = collection.find()
-    print("Retrieved all documents successfully!")
-except Exception as e:
-    print("Unable to retrieve documents: ", e)
-
-# Iterate through each document and print its contents
-try:
-    for doc in documents:
-        product = {
-            'type_of_product': doc['type'],
-            'brand': doc['brand'],
-            'description': doc['description'],
-            'name': doc['name'],
-            'photos': doc['photos'],
-            'color': [doc['color']],
-            'gender': doc['gender'],
-            'price': doc['price'],
-            'link': doc['_id'],  # TODO add link to product
-        }
-
-        add_dict_to_file('clothes.txt', product)
-
-
-except Exception as e:
-    print("Error processing documents: ", e)
+if __name__ == '__main__':
+    shoes = connect_db('products', 'shoes')
+    write_to_file(shoes, 'shoes.txt')
+    clothes = connect_db('products', 'clothes')
+    write_to_file(clothes, 'clothes.txt')
